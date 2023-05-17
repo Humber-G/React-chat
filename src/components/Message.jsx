0cd3bed76e60b8507ac6b25a90694f34294727ab
Humberto-G <humbertoandres85@gmail.com>
2023-05-17 12:17:58 -0400

    Initial layout complete

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Message = () => {
-	return <div>Message</div>;
+	return (
+		<div className='message owner'>
+			<div className='messageInfo'>
+				<img
+					src='https://media.istockphoto.com/id/184941527/es/foto/hombre-de-negocios-en-ropa-chimpanc%C3%A9.jpg?b=1&s=612x612&w=0&k=20&c=as2MUbBzLoSVdfzJHg1S3h2SAET_pS_DtNN-KFcPgHQ='
+					alt=''
+				/>
+				<span>just now</span>
+			</div>
+			<div className='messageContent'>
+				<p>hello</p>
+				<img
+					src='https://media.istockphoto.com/id/184941527/es/foto/hombre-de-negocios-en-ropa-chimpanc%C3%A9.jpg?b=1&s=612x612&w=0&k=20&c=as2MUbBzLoSVdfzJHg1S3h2SAET_pS_DtNN-KFcPgHQ='
+					alt=''
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Message;
