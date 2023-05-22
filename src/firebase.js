9848c194947d3db1c2ec207b571a941194754d80
Humberto-G <humbertoandres85@gmail.com>
2023-05-22 14:36:39 -0400

    Added firebase integration

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = process.env.REACT_APP_MESSAGING_SENDER_ID;
+const APP_ID = process.env.REACT_APP_APP_ID;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: APP_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
