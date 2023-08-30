5f3307c511bf2678e9158b1a79153f12447715df
Humberto-G <humbertoandres85@gmail.com>
2023-08-30 12:49:28 -0400

    Added chatContext

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,40 +1,50 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { doc, onSnapshot } from 'firebase/firestore';
-import { db } from '../firebase';
-import { AuthContext } from '../context/AuthContext';
+import React, { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
-	const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState([]);
 
-	const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
-	useEffect(() => {
-		const getChats = () => {
-			const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
-				setChats(doc.data());
-			});
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
 
-			return () => {
-				unsub();
-			};
-		};
+      return () => {
+        unsub();
+      };
+    };
 
-		currentUser.uid && getChats();
-	}, [currentUser.uid]);
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
 
-	return (
-		<div className='chats'>
-			{Object.entries(chats)?.map((chat) => (
-				<div className='userChat' key={chat[0]}>
-					<img src={chat[1].userInfo.photoURL} alt='' />
-					<div className='userChatInfo'>
-						<span>{chat[1].userInfo.displayName}</span>
-						<p>{chat[1].userInfo.lastMessage?.text}</p>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
+  return (
+    <div className="chats">
+      {Object.entries(chats)?.map((chat) => (
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Chats;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
-	return (
-		<div className='message owner'>
-			<div className='messageInfo'>
-				<img
-					src='https://media.istockphoto.com/id/184941527/es/foto/hombre-de-negocios-en-ropa-chimpanc%C3%A9.jpg?b=1&s=612x612&w=0&k=20&c=as2MUbBzLoSVdfzJHg1S3h2SAET_pS_DtNN-KFcPgHQ='
-					alt=''
-				/>
-				<span>just now</span>
-			</div>
-			<div className='messageContent'>
-				<p>hello</p>
-				<img
-					src='https://media.istockphoto.com/id/184941527/es/foto/hombre-de-negocios-en-ropa-chimpanc%C3%A9.jpg?b=1&s=612x612&w=0&k=20&c=as2MUbBzLoSVdfzJHg1S3h2SAET_pS_DtNN-KFcPgHQ='
-					alt=''
-				/>
-			</div>
-		</div>
-	);
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src="" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
